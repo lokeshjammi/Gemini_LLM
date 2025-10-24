6d08da8a854b41323dc5419101f88ae2acac3c8e
lokeshjammi <jammilokesh3@gmail.com>
2025-10-24 23:19:56 +0530

    Multiple params working code commit

--- function_calling_gemini.py
+++ function_calling_gemini.py
@@ -14,6 +14,8 @@ else:
 system_instruction = "You're an travel advisor and you only focus to answer for questions related to travel."
 system_instruction += "When user asks for one way ticket fare you can call the required tool from tool declarations, otherwise if that is a general query, response"
 "should be given normally and if the query is not related to tarvel, please respond like couldn't able to answer other than travelling related questions."
+
+#ticket fare for the given city
 def check_price_of_ticket(destination_city: list[str]):
     result = {}
     ticket_fare = {
@@ -59,7 +61,7 @@ ticket_calculator_function = {
 
 #configure the LLM Model with all required things
 client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
-user_prompt = "Please give me ticket fare for Chennai, Bangalore seperately"
+user_prompt = "What's the ticket fare to Chennai and Dubai?"
 tools = types.Tool(function_declarations=[ticket_calculator_function])
 config = types.GenerateContentConfig(tools=[tools], system_instruction=system_instruction)
 
@@ -68,18 +70,22 @@ response = client.models.generate_content(model='gemini-2.0-flash', config=confi
                                           contents=user_prompt)
 
 if response.candidates[0].content.parts[0].function_call:
+    llm_response_list = []
     function_call = response.candidates[0].content.parts[0].function_call
     function_name = function_call.name
     function_args = function_call.args
     if function_name == "check_price_of_ticket":
         result = check_price_of_ticket(destination_city=function_args.get('destination_city'))
         result_list = list(result.values())
-        print(result)
-    response = client.models.generate_content(
-        model='gemini-2.0-flash',
-        contents = f"The ticket fare for {function_args.get('destination_city')} is {result}",
-        config = None
-    )
-    print(response.candidates[0].content.parts[0].text)
+    for i in range(len(result_list)):
+        city = function_args.get('destination_city')[i]
+        result = result_list[i]
+        response = client.models.generate_content(
+            model='gemini-2.0-flash',
+            contents = f"The ticket fare for {city} is {result}",
+            config = None
+        )
+        llm_response_list.append(response.candidates[0].content.parts[0].text)
+        print(response.candidates[0].content.parts[0].text)
 else:
     print(response.candidates[0].content.parts[0].text)
