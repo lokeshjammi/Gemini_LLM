f341d1ea3c51840df9c830965f3467b86932657c
lokeshjammi <jammilokesh3@gmail.com>
2025-10-27 22:42:23 +0530

    Gemini basic chat working code

--- chat_function_calling_gemini.py
+++ chat_function_calling_gemini.py
@@ -1,5 +1,4 @@
 from google import genai
-from google.genai import types
 from dotenv import load_dotenv
 import os
 import gradio as gr
@@ -15,13 +14,21 @@ else:
 
 client = genai.Client(api_key=api_key)
 
-system_instruction = "You're an travel advisor and you only focus to answer for questions related to travel."
-system_instruction += "When user asks for one way ticket fare you can call the required tool from tool declarations, otherwise if that is a general query, response"
-"should be given normally and if the query is not related to tarvel, please respond like couldn't able to answer other than travelling related questions."
-
-config = types.GenerateContentConfig(system_instruction=system_instruction)
-
 model = client.chats.create(
-    model = "gemini-2.0-flash",
-    config=config
+    model="gemini-2.0-flash",
+    history=[]
 )
+
+while True:
+    user_input = input("User: ")
+    
+    if user_input.lower() in ['exit', 'quit']:
+        print("Exiting the chat.")
+        break
+
+    response = model.send_message(user_input)
+
+    if response.candidates[0].content.parts[0].function_call:
+        pass
+    else:
+        print("\n"+"Gemini:", response.text)
